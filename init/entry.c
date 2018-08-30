39c777d35f747fe2255df56f776ed7e8d1fa6533
zhangzhihe <zhangzhihe@ofo.com>
2018-08-30 23:17:12 +0800

    b

--- init/entry.c
+++ init/entry.c
@@ -1,8 +1,52 @@
 #include "types.h"
 
+
+typedef
+enum real_color {
+    rc_black = 0,
+    rc_blue = 1,
+    rc_green = 2,
+    rc_cyan = 3,
+    rc_red = 4,
+    rc_magenta = 5,
+    rc_brown = 6,
+    rc_light_grey = 7,
+    rc_dark_grey = 8,
+    rc_light_blue = 9,
+    rc_light_green = 10,
+    rc_light_cyan = 11,
+    rc_light_red = 12,
+    rc_light_magenta = 13,
+    rc_light_brown  = 14,   // yellow
+    rc_white = 15
+} real_color_t;
+
+
+// VGA 的显示缓冲的起点是 0xB8000
+static uint16_t *video_memory = (uint16_t *)0xB8000;
+
+// 屏幕"光标"的坐标
+static uint8_t cursor_x = 0;
+static uint8_t cursor_y = 0;
+
+void console_clear()
+{
+    uint8_t attribute_byte = (0 << 4) | (15 & 0x0F);
+    uint16_t blank = 0x20 | (attribute_byte << 8);
+
+    int i;
+    for (i = 0; i < 80 * 25; i++) {
+        video_memory[i] = blank;
+    }
+
+    cursor_x = 0;
+    cursor_y = 0;
+    move_cursor();
+}
+
 int kern_entry()
 {
-
+    console_clear();
     uint8_t *input = (uint8_t *)0xB8000;
     uint8_t color = (0 << 4) | (15 & 0x0A);
     //1111 & 0000 1111
