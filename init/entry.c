6dc5ff6fd3ca7b92efbd9c2fb0cfe41620a8e6e2
zhangzhihe <zhangzhihe@ofo.com>
2018-08-30 22:56:09 +0800

    a

--- init/entry.c
+++ init/entry.c
@@ -4,7 +4,8 @@ int kern_entry()
 {
 
     uint8_t *input = (uint8_t *)0xB8000;
-    uint8_t color = (0 << 4) | (15 & 0x0F);
+    uint8_t color = (0 << 3) | (15 & 0x0F);
+    //1111 & 0000 1111
 
     *input++ = 'H'; *input++ = color;
     *input++ = 'e'; *input++ = color;
